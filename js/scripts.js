c62bd185dd317a0b637ba76bc82999b66669d56c
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-14 16:52:40 -0600

    add forms and buttons

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("form#formOne").submit(function(event) {
+    const color = $("input:radio1[name=color]:checked").val();
+    const activity = $("input:radio2[name=activity]:checked").val();
+    const food = $("input:radio3[name=food]:checked").val();
+
+
+
+});
