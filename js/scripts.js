a5ecd0293ad58691eb59adab81bf0e2d520ae355
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-15 10:00:33 -0600

    add project with form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-  $("form#formOne").submit(function(event) {
-    const color = $("input:radio1[name=color]:checked").val();
-    const activity = $("input:radio2[name=activity]:checked").val();
-    const food = $("input:radio3[name=food]:checked").val();
+  // $("form#formOne").submit(function (event) {
+  const color = $("input:radio[name=color]:checked").val();
+  // const activity = $("input:radio2[name=activity]:checked").val();
+  // const food = $("input:radio3[name=food]:checked").val();
 
-
-
+  if (color === red) {
+    $("#spongeBob").show();
+  } else {
+    $("#shakira").show();
+  }
+  // event.preventDefault();
+  // });
 });
